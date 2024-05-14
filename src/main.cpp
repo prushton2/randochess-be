aaa98697e6dfbc2231d544a6b9dc964296a88e00
prushton2 <peterrushton418@gmail.com>
2024-05-14 08:47:38 -0400

    cors exists just to make me suffer

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,14 @@ using std::cout;
 using std::endl;
 using std::string;
 
+void set_headers(Response* res) {
+	res->set_header("Access-Control-Allow-Origin", "*");
+	res->set_header("Access-Control-Allow-Credentials", "true");
+	res->set_header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, HEAD");
+	res->set_header("Allow", "GET, POST, HEAD, OPTIONS");
+	res->set_header("Access-Control-Allow-Headers", "*"); //X-Requested-With, Content-Type, Accept, Origin, Authorization");
+}
+
 int main(void)
 {
 	// Definitions
@@ -27,43 +35,48 @@ int main(void)
 	//Init game
 	//Send back a join code to a game for the opponent, and a host code for the host
 	
+
+
 	svr.Get("/game/new", [&](const Request &req, Response &res) {
-			//create 2 codes, one for host to play and one for guest to play
-			cout << distribution(generator) << " was generated" << endl;
-			string host_code = std::to_string(distribution(generator));
-			string guest_code = std::to_string(distribution(generator));
-			
+		//create 2 codes, one for host to play and one for guest to play
+		cout << distribution(generator) << " was generated" << endl;
+		string host_code = std::to_string(distribution(generator));
+		string guest_code = std::to_string(distribution(generator));
+		
 
-			while(gamecodes[host_code] != 0) {
-				host_code = std::to_string(distribution(generator));
-			}
-			while(gamecodes[guest_code] != 0) {
-				guest_code = std::to_string(distribution(generator));
-			}
-			gamecodes[host_code] = new Game;
-			gamecodes[guest_code] = gamecodes[host_code];
+		while(gamecodes[host_code] != 0) {
+			host_code = std::to_string(distribution(generator));
+		}
+		while(gamecodes[guest_code] != 0) {
+			guest_code = std::to_string(distribution(generator));
+		}
+		gamecodes[host_code] = new Game;
+		gamecodes[guest_code] = gamecodes[host_code];
 
-			gamecodes[host_code]->init_board();
+		gamecodes[host_code]->init_board();
 
-			cout << "Host: " << host_code << endl;
-			cout << "Guest: " << guest_code << endl;
+		cout << "Host: " << host_code << endl;
+		cout << "Guest: " << guest_code << endl;
+		
+		set_headers(&res);
 
-			res.set_content(
-				"{'host_code':"+host_code+",'guest_code':"+guest_code+"}", 
-			"text/json");
+		res.set_content(
+			"{\"host_code\":"+host_code+",\"guest_code\":"+guest_code+"}", 
+		"application/json");
 	});
 
 	svr.Get(R"(/game/info/([0-9][0-9]*))", [&](const Request &req, Response &res) {
                 string code = req.matches[1];
+		set_headers(&res);
 		res.set_content("HAHA!", "text/plain");
 	});
 
 	//Join game
-	//Check if join code is valid, if so the relevane info of the game is returned
-	
-	//Send move
-	//Sends: start position, end position
-	//Receives: Is valid move
+	//Check if join code is valid	
+	svr.Get(R"(/game/exists/([0-9][0-9]*))", [&](const Request &req, Response &res) {
+		set_headers(&res);
+		res.set_content( std::to_string(gamecodes[req.matches[1]] != NULL), "text/plain");
+	});
 	
 	//Fetch Move
 	svr.Get(R"(/game/fetch/([0-9][0-9]*))", [&](const Request &req, Response &res) {
@@ -79,8 +92,11 @@ int main(void)
 		res.set_content(board_state, "text/plain");
 	});
 
-	//IsValidMove
-	//Since the movable spots can be undetermined, this move takes two spots and determines if the piece at p1 can move to p2
+	//Send Move
+	svr.Post(R"(/game/move/([0-9][0-9]*))", [&](const Request &req, Response &res) {
+		cout << req.body << endl;
+		res.set_content(req.body, "text/plain");
+	});
 
 	//GetValidMoves
 	//returns the spots the piece at pos1 can move to
